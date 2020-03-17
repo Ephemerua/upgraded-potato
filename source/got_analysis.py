c87916b8f528106acde756525e5c400533643b0c
Ephem <842825214@qq.com>
2020-03-17 19:32:52 +0800

    now heap analyse and got analyse works

--- source/got_analysis.py
+++ source/got_analysis.py
@@ -3,13 +3,13 @@ from pwnlib.elf.elf import ELF
 
 # TODO: test this function
 class got_analysis(object):
-    def __inif__(self, project):
+    def __init__(self, project):
         self.project = project
 
     def do_analysis(self):
         # first we resolve all imported symbols' addr
         main = self.project.elfs["main"]
-        orirgin_got = {}
+        origin_got = {}
         exploited_got = {}
         for sym in main.got:
             # how to judge which file a symbol belongs to ??? 
@@ -18,12 +18,15 @@ class got_analysis(object):
                 if libname == "main":
                     continue
                 if sym in obj.symbols:
-                    origin_got[sym] = obj.symbols[sym]
-                    break
+                    if sym in origin_got:
+                        origin_got[sym].append(obj.symbols[sym])
+                    else:
+                        origin_got[sym] = [obj.symbols[sym]]
+                    
 
             # then save exploited got to dict
             addr = main.got[sym]
-            sym_addr = self.project.exploied_state.memory.load(addr, 8, endness = 'Iend_LE')
+            sym_addr = self.project.exploited_state.memory.load(addr, 8, endness = 'Iend_LE')
             assert(sym_addr.concrete)
             sym_addr = sym_addr.args[0]
             exploited_got[sym] = sym_addr
@@ -35,14 +38,18 @@ class got_analysis(object):
         assert(len(origin_got) == len(exploited_got))
         for sym, addr in exploited_got.items():
             # check if addr matched, or the symbol haven't been resolved 
-            if origin_got[sym] == addr:
+            # don't track 0 addr
+            if addr == 0:
                 continue
-            elif main.plt[sym] == addr:
+            if addr in origin_got[sym]:
                 continue
-            else:
-                #TODO: do report
-                print("Found got mismatch: symbol %s with addr %s" % (sym, hex(addr)) )
-                pass
+            elif sym in main.plt:
+                if main.plt[sym] == addr:
+                    continue
+                else:
+                    #TODO: do report
+                    print("Found got mismatch: symbol %s with addr %s" % (sym, hex(addr)) )
+                    pass
 
             
         
--- source/heap_analysis.py
+++ source/heap_analysis.py
@@ -1,29 +1,160 @@
-def malloc_hook(state):
+def _malloc_hook(state):
+    """
+    malloc hook.
+    Get size passed to malloc, and set a hook on caller's return address to get malloc's 
+    return value.
+    After getting the return value, unhook the return address.
+    """
     # get return address and arg
     size = state.regs.rdi
     assert(size.concrete)
     size = size.args[0]
+    size = ((size>>4)<<4) + 0x10
+
     rsp = state.regs.rsp
     assert(rsp.concrete)
     # malloc use rsp to save return address...
     ret_addr = state.memory.load(rsp, 8, endness = 'Iend_LE')
     assert(ret_addr.concrete)
     ret_addr = ret_addr.args[0]
-    print(hex(ret_addr))
+    #print(hex(ret_addr))
 
-    def malloc_callback(state):
+    def _malloc_callback(state):
+        # rax contains return address of malloc
+        rax = state.regs.rax
+        assert(rax.concrete)
+        rax = rax.args[0]
+        # TODO: do log
+        print("Malloc called with size %d, returns addr %s" % (size, hex(rax)))
+        state.project.unhook(ret_addr)
+        state.project.heap_analysis.add_chunk(rax, size)
+
+    assert(not state.project.is_hooked(ret_addr))
+    state.project.hook(ret_addr, _malloc_callback)
+
+def _calloc_hook(state):
+    """
+    Same with _malloc_hook
+    """
+    # get return address and arg
+    count = state.regs.rdi
+    assert(count.concrete)
+    count = count.args[0]
+
+    size = state.regs.rsi
+    assert(size.concrete)
+    size = size.args[0]
+    size = ((size>>4)<<4) + 0x10
+
+
+    rsp = state.regs.rsp
+    assert(rsp.concrete)
+    # malloc use rsp to save return address...
+    ret_addr = state.memory.load(rsp, 8, endness = 'Iend_LE')
+    assert(ret_addr.concrete)
+    ret_addr = ret_addr.args[0]
+
+    def _calloc_callback(state):
         # rax contains return address of malloc
         rax = state.regs.rax
         assert(rax.concrete)
         #rax = rax.args[0]
         # TODO: do log
-        print("Malloc called with size %d, returns addr %s" % (size, rax))
+        print("Calloc called with size %d, returns addr %s" % (size*count, hex(rax)))
         state.project.unhook(ret_addr)
 
     assert(not state.project.is_hooked(ret_addr))
-    state.project.hook(ret_addr, malloc_callback)
+    state.project.hook(ret_addr, _calloc_callback)
 
+def _free_hook(state):
+    # get addr and chunk_size
+    addr = state.regs.rdi
+    assert(addr.concrete)
+    addr = addr.args[0]
+
+    size = state.memory.load(addr-8, 8, endness = 'Iend_LE')
+    assert(size.concrete)
+    size = size.args[0]
+    size = (size >> 4)<<4
+    state.project.heap_analyse.del_chunk(addr, size)
+
+
+
+
+
+class heap_analysis(object):
+    """
+    Analyses heap operations.
+    Hooks the top level api(malloc, free, etc.), get information and then use read/write
+    breakpoints to do analysis.
+
+    :ivar chunks_av:        dict of allocated chunks, indexed by address
+    :ivar chunks_sv:        dict of allocated chunks, indexed by size
+    :ivar abused_chunks:    list of chunks been abused, chunk sample: 
+                            {"addr": 0x603000, "size":0x68, "type": abused_type}
+
+    :ivar arenas:           list of thread_arena structs # TODO: haven't do the parse job
+    """
+    def __init__(self, project, segments=[], ):
+        self.project = project
+        self.heap_segments = segments
+        self.malloc_hook = _malloc_hook
+        self.calloc_hook = _calloc_hook
+        self.free_hook = _free_hook
+        self.chunks_av = {}
+        self.chunks_sv = {}
+        self.abused_chunks = []
+        self.arenas = []
     
+    def add_chunk(self, addr, size):
+        # do log in chunks_av
+        if addr in self.chunks_av:
+            # why allocated again?
+            sizes = self.chunks_av[addr]
+            if isinstance(sizes, list):
+                size.append(size)
+            else:
+                self.chunks_av[addr] = [sizes, size]
+            self.abused_chunks.append({"addr":addr, "size":size, "type":"allocated mutiple times"})
+        else:
+            self.chunks_av[addr] = size
+            
+        # do log in chunks_sv
+        if size in self.chunks_sv:
+            self.chunks_sv[size].append(addr)
+        else:
+            self.chunks_sv[size] = [addr]
+
+    def del_chunk(self, addr, size):
+        # check if the chunk is allocated
+        if addr in self.chunks_av:
+            sizes = self.chunks_av[addr]
+            # check if size matchess
+            if isinstance(sizes, list):
+                if size in sizes:
+                    sizes.remove(size)
+                    if sizes==[]:
+                        self.chunks_av.pop(addr)
+                else:
+                    self.abused_chunks.append({"addr": addr, "size":size, "type":"freed with modified size"})
+            # target chunk doesn't been alloced more than one time,
+            # so sizes is an int
+            elif size == sizes:
+                self.chunks_av.pop(addr)
+            # do log in chunks_sv
+            if size in self.chunks_sv:
+                if addr in self.chunks_sv[size]:
+                    self.chunks_sv[size].remove(addr)
+        else:
+            # this chunk is not alloced by c/m/relloc
+            self.abused_chunks.append({"addr":addr, "size": size, "type":"chunk not allocated is freed"})
+
+
+
+                
+
+
+
 
 
 
