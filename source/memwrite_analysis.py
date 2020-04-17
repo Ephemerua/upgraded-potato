a6bf7d70e216eaf36002dd9c2037c9dba4127d6c
NemoYuan2008 <542629877@qq.com>
2020-04-18 00:51:40 +0800

    Fixed the incorrect results in memwrite_analysis.py Still some troubles present

--- source/memwrite_analysis.py
+++ source/memwrite_analysis.py
@@ -2,28 +2,32 @@ from replayer import Replayer
 
 
 class memwrite_analysis(object):
-    # TODO: addr can also be claripy.BVVs besides integers
+    # TODO: maybe addr can also be claripy.BVVs besides integers
     def __init__(self, project: Replayer, *addr):
         self.__project = project
-        self.__addr = list(addr)
+        self.__addr = set(addr)
         self.__state = self.__project.factory.entry_state()
 
     def __set_bp(self):
         def memwrite_bp_callback(state):
+            nonlocal addr
             write_addr = state.inspect.mem_write_address
-
-            # FIXME: result is incorrect, is it a bug of angr or due to alignment???
-            # FIXME: eg: it yields 0x8 rather than 0x10, 0x10 rather than 0x20
             write_length = state.inspect.mem_write_length
             print('Memory wrote to', write_addr, 'length', write_length)
+            print('Contents', state.inspect.mem_write_expr)
+            # TODO: doing the job recursively is too ugly and slow, can we do it more elegantly?
+            # FIXME: we run into trouble here, generating too many incomprehensible and annoying error messages
+            #  despite the fact that the results are correct
+            m = memwrite_analysis(self.__project, addr + write_length.args[0])
+            m.do_analysis()
 
         for addr in self.__addr:
-            # TODO: deal with excessive breakpoints so that the same breakpoint can't be added more than once
-            # Do we need to do the above???
             self.__state.inspect.b('mem_write', mem_write_address=addr, action=memwrite_bp_callback)
 
     def add_addr(self, addr):
-        self.__addr.append(addr)
+        if not isinstance(addr, int):
+            raise TypeError('Argument passed to add_addr must be an int')
+        self.__addr.add(addr)
 
     def do_analysis(self):
         """
