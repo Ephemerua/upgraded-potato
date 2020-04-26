b17c7c642cb152a4f2f40e25cb6fe914b47f73d1
Ephem <842825214@qq.com>
2020-04-26 20:00:45 +0800

    clean some code

--- source/heap_analysis.py
+++ source/heap_analysis.py
@@ -7,7 +7,6 @@ from arena import Arena
 TODO: move this part to doc
 Heap analysis, focus on alloc, free and read/write on heap.
 
-
 To detect overflow:
 Backward overflow:
     A write with size exceeding the size of chunk.
@@ -81,13 +80,10 @@ def printable_memory(state, start, size, warn_pos = 0, warn_size = 0, info_pos =
             j += 1
 
         result += "".join(colored_mem) 
-
         if endl:
             result += '\n'
     return result
             
-
-
 
 def bp_overflow(start_addr, size, callback = None, debug = False):
     def write_bp(state):
@@ -174,41 +170,6 @@ def bp_redzone(start_addr, size, callback = None, debug = False, allow_heap_ops 
         return
     return write_bp
     
-#de
-def bp_constructor(start_addr, size, callback = None, debug = False):
-    """
-    construct an state.inspect.bp, record mem write to detect overflow
-    [start_addr, start_addr + size) is the region we can access.
-
-    """
-    def write_bp(state):
-        target_addr = state.inspect.mem_write_address
-        target_size = state.inspect.mem_write_length
-        if type(target_addr) != int:
-            target_addr = target_addr.args[0]
-        if type(target_size) != int:
-            target_size = target_size.args[0]
-
-        
-        if (target_addr >= start_addr + size) \
-            or (start_addr >= target_addr + target_size):
-            return
-
-        
-        if debug:
-            print("mem write to %s with length %s" % (hex(target_addr), hex(target_size)))
-        if (target_addr + target_size > start_addr + size):
-            print("Found overflow in chunk at %s, size %s with write starts at %s, size %s!"
-             % (hex(start_addr), hex(size), hex(target_addr), hex(target_size)))
-            overflow_len = target_addr + target_size - (start_addr + size)
-            #print("Overflow length: %s" % hex(overflow_len))
-            overflow_content = state.inspect.mem_write_expr[overflow_len*8 - 1:0]
-            print("Overflow length: %s, content: %s" % (hex(overflow_len), overflow_content))
-        return
-    return write_bp
-
-
-
 
 def _malloc_hook(state):
     """
@@ -267,9 +228,7 @@ def _malloc_hook(state):
         if rax in bps:
             state.inspect.remove_breakpoint(event_type = 'mem_write', bp = bps[rax])
             state.project.heap_analysis.free_bps.pop(rax)
-
-
-        
+    
         bp_content = state.inspect.b("mem_write", action = bp_overflow(rax, origin_size))
         bp_metadata = state.inspect.b("mem_write", action = \
             bp_redzone(rax-0x10, 0x10, allow_heap_ops = False, mtype = 'chunk header'))
@@ -279,6 +238,7 @@ def _malloc_hook(state):
     assert(not state.project.is_hooked(ret_addr))
     state.project.hook(ret_addr, _malloc_callback)
 
+# TODO： rewrite this!!!!
 def _calloc_hook(state):
     """
     Same with _malloc_hook
@@ -372,166 +332,6 @@ def _free_hook(state):
     state.project.hook(ret_addr, _free_callback)
 
 
-def ptr_check(ptr, reverse_maps, state = None):
-    page = ptr >> 12
-    prot = -1
-    if state:
-        try:
-            prot = state.memory.permissions(ptr)
-            return prot.args[0]
-        except angr.errors.SimMemoryMissingError:
-            return -1
-    else:
-        if page in reverse_maps:
-            return reverse_maps[page]
-        else:
-            return -1
-    
-def ptr_lookup(ptr, reverse_maps, state = None):
-    prot = ptr_check(ptr, reverse_maps, state)
-    if prot == -1:
-        return -1
-    else:
-        if ptr in reverse_maps:
-            return reverse_maps[ptr]
-        else:
-            return None
-
-def get_malloc_state(state, addr = 0):
-    """
-    TODO: test this func
-    get struct malloc_state from memory
-    still we need the address of the struct
-    """
-    main_arena_addr = state.project.symbol_resolve.resolve("__malloc_hook") + 0x10
-    result = malloc_state()
-    mem = state.memory.load(main_arena_addr, result.size)
-    mem = mem.args[0].to_bytes(result.size, 'big')
-    result.unpack(mem)
-    main_arena = malloc_state()
-    main_arena.unpack(mem)
-    # if addr is in main thread's heap, addr shouldn't be on the same segment 
-    # of malloc_state's address. But if it's on thread heap, addr should be on
-    # the same seg with thread arena.
-    if main_arena_addr == result.next:
-        # we have only one arena! just return it
-        return result, main_arena_addr
-    else:
-        # consider thread arena
-        # TEST: multi-thread support haven't been tested!
-        addr >>= 20
-        while result.next != main_arena_addr and result.next != 0:
-            # go to next arena
-            temp_addr = result.next
-            mem = state.memory.load(result.next, result.size)
-            mem = mem.args[0].to_bytes(result.size, 'big')
-            result.unpack(mem)
-            if temp_addr >> 20 == addr:
-                # we found it!
-                return result, temp_addr
-            else:
-                continue
-        # there's no match arena, so maybe given addr is on main thread's heap，
-        # or it is an abused addr
-        # we cannot determine...
-        #print("Unable to find thread arena with addr %s!" % hex(addr))
-        return main_arena, main_arena_addr
-            
-
-
-def single_list_iterate(state, link_head, ptr_offset): 
-    """
-    iterate a single list, until it meets 0 or a circle.
-    Returns a list of all nodes the list have, including the head node,
-    and if it is a circler list.
-
-    :param state:       the state to get memory from
-    :param link_head:   head of the list
-    :ptr_offset:        offset of fd(next) ptr in the list's struct
-    """
-    circlar_node = 0
-    bin_list = [link_head]
-    link_head = state.memory.load(link_head+ptr_offset, 8, endness = 'Iend_LE').args[0]
-    while link_head!=0:
-        # a circle?
-        if link_head in bin_list:
-            circlar_node = link_head
-            break
-        bin_list.append(link_head)
-        link_head = state.memory.load(link_head+ptr_offset, 8, endness = 'Iend_LE').args[0]
-    return bin_list, circlar_node
-
-def bin_fd_iterate(state, link_head):
-    ptr_offset = 0x10
-    return single_list_iterate(state, link_head, ptr_offset)
-
-def bin_bk_iterate(state, link_head):
-    ptr_offset = 0x18
-    return single_list_iterate(state, link_head, ptr_offset)
-
-def printable_fastbin_entry(nodes, circlar = 0, size = 0):
-    result = "fastbin"
-    if size:
-        result += "[%s]"  % hex(size)
-    result += ": "
-    for node in nodes:
-        result += hex(node) + " -> "
-    if circlar:
-        result += hex(circlar) + "(corrupted)"
-    else:
-        result += '0\n'
-    return result
-
- # TODO: rewrite   
-def fastbin_check(state, malloc_state, arena_addr):
-    """
-    fastbin is a single list, which shouldn't contain circle.
-
-    """
-    #output = ""
-    for link_head in malloc_state.fastbinsY:
-        if link_head == 0:
-            continue
-        nodes, circlar = bin_fd_iterate(state, link_head)
-        for node in nodes:
-            owner = ptr_lookup(node, state.project.reverse_maps, state)
-            if owner == -1:
-                print("Found unmapped address in fastbin.")
-            elif owner:
-                if owner[0]:
-                    print("Found fastbin points to %s !" % owner[0])
-        if circlar:
-            print("Found corrupted fastbin!")
-        print(printable_fastbin_entry(nodes, circlar))
-
-def printable_bin_entry(fd_nodes, bk_nodes, size = 0):
-    result = "bin"
-    if size:
-        result += "[%s]"  % hex(size)
-    result += ": \n"
-    for node in fd_nodes:
-        result += hex(node) + " -> "
-    result += '\n'
-    for node in bk_nodes:
-        result += hex(node) + " -> "
-    return result
-
-#TODO: rewrite
-def bin_check(state, malloc_state, arena_addr):
-    # seems we have to get arena address...
-    arena_size = malloc_state.size
-    for i in range(2, len(malloc_state.bins), 2):
-        fd, bk = malloc_state.bins[i:i+2]
-        fd_nodes, fd_circlar = bin_fd_iterate(state, fd)
-        bk_nodes, bk_circlar = bin_bk_iterate(state, bk)
-        if len(fd_nodes) == 1 and fd_nodes[0] == fd_circlar:
-           if len(bk_nodes) == 1 and bk_nodes[0] == bk_circlar:
-               continue
-        print(printable_bin_entry(fd_nodes + [fd_circlar], bk_nodes + [bk_circlar]))
-    pass
-
-def unsortedbin_check(state, malloc_state, arena_addr):
-    pass
 class heap_analysis(object):
     """
     Analyses heap operations.
@@ -626,7 +426,8 @@ class heap_analysis(object):
         # hook heap api, save address to hooked_addr
         self.hooked_addr.append(self.project.hook_symbol("malloc", self.malloc_hook))
         self.hooked_addr.append(self.project.hook_symbol("free", self.free_hook))
-        self.hooked_addr.append(self.project.hook_symbol("calloc", self.calloc_hook))
+        # calloc still in work
+        #self.hooked_addr.append(self.project.hook_symbol("calloc", self.calloc_hook))
 
     def disable_hook(self):
         # clean hooks by saved addr
